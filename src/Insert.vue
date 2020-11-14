7b530f772e4a022f90c7d4ce59f68b15cb1758b9
Yifan Jiang <jimmyjiang1999@gmail.com>
2020-11-14 17:04:17 +0800

    差值

---
+++ src/Insert.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <p><label>变量:</label> {{ num }}</p>
+    <p><label>表达式:</label> {{ 5 + 10 }}</p>
+    <p><label>条件运算符:</label> {{ true ? 5 : 10 }}</p>
+  </div>
+</template>
+
+<script>
+import { ref } from "vue";
+
+export default {
+  setup() {
+    const num = ref("2");
+
+    return {
+      num,
+    };
+  },
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -2,8 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css'
 import Data from './Data.vue'
+import Insert from './Insert.vue'
 
 //createApp(App).mount('#app')
-createApp(Data).mount('#app')
+//createApp(Data).mount('#app')
+createApp(Insert).mount('#app')
 
 
