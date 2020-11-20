986b415323e51d954a981f937473ca23b8e7aa35
Yifan Jiang <jimmyjiang1999@gmail.com>
2020-11-21 00:36:45 +0800

    vmodel

---
+++ src/components/vmodel.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <input v-model="message" placeholder="请输入" />
+    <p>Message is: {{ message }}</p>
+    <br />
+
+    <!--多行文本-->
+    <textarea v-model="mulMessage" placeholder="请输入"></textarea>
+    <br />
+    <span>Multiline message is:</span>
+    <p style="white-space: pre-line">{{ mulMessage }}</p>
+  </div>
+</template>
+
+<script>
+import { reactive, ref, toRefs } from "vue";
+export default {
+  setup() {
+    const data = reactive({
+      message: "",
+      mulMessage: "",
+    });
+
+    return {
+      ...toRefs(data),
+    };
+  },
+};
+</script>
+
+<style></style>
--- src/main.js
+++ src/main.js
@@ -5,11 +5,13 @@ import Data from './Data.vue'
 import Insert from './Insert.vue'
 import Computed from './components/Compute.vue'
 import Watch from './components/watch.vue'
+import Vmodel from './components/vmodel.vue'
 
 //createApp(App).mount('#app')
 //createApp(Data).mount('#app')
 //createApp(Insert).mount('#app')
 //createApp(Computed).mount('#app')
-createApp(Watch).mount('#app')
+//createApp(Watch).mount('#app')
+createApp(Vmodel).mount('#app')
 
 
