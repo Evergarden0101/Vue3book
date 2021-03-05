1825d8a3e3dcaaf5dd28104ee1e2ce85c2646e29
Yifan Jiang <jimmyjiang1999@gmail.com>
2021-03-06 00:45:01 +0800

    路由

--- router-proj/src/router/index.js
+++ router-proj/src/router/index.js
@@ -1,35 +1,22 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from "../views/Home.vue";
-
-// import { createRouter, createWebHashHistory } from "vue-router";
-
-Vue.use(VueRouter)
+import { createRouter, createWebHashHistory } from 'vue-router'
+import Home from '../views/Home.vue'
 
 const routes = [
   {
-    path: "/",
-    name: "Home",
+    path: '/',
+    name: 'Home',
     component: Home
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+    path: '/about',
+    name: 'About',
+    component: () => import('../views/About.vue')
   }
-];
+]
 
-const router = new VueRouter({
+const router = createRouter({
+  history: createWebHashHistory(),
   routes
-});
+})
 
-// const router = createRouter({
-//   history: createWebHashHistory(),
-//   routes
-// });
-
-export default router;
+export default router
